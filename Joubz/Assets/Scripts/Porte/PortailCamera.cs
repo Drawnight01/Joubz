217ef71cfacd66b02756e6c0252dfba8669a4b35
Drawnight01 <71324732+Drawnight01@users.noreply.github.com>
2022-04-16 19:05:52 +0200

    commit

---
+++ Joubz/Assets/Scripts/Porte/PortailCamera.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortailCamera : MonoBehaviour
+{
+    public Transform playerCamera, portal, otherPortal;
+
+    void Update()
+    {
+        Vector3 playerOffsetFromPortal = playerCamera.position - otherPortal.position;
+        transform.position = portal.position + playerOffsetFromPortal;
+
+        float angularDifferenceBetweenPortalRotations = Quaternion.Angle(portal.rotation, otherPortal.rotation);
+
+        Quaternion portalRotationDifference = Quaternion.AngleAxis(angularDifferenceBetweenPortalRotations, Vector3.up);
+        Vector3 newCameraDirection = portalRotationDifference * playerCamera.forward;
+        transform.rotation = Quaternion.LookRotation(newCameraDirection, Vector3.up);
+    }
+}
